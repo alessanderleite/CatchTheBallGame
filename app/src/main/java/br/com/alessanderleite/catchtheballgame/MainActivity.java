8a62fe706812349f202febf655a5bc7e5127f453
alessanderleite <alessander.logistica@gmail.com>
2019-02-02 14:15:29 -0200

    finishing move the Box Up and Down

--- app/src/main/java/br/com/alessanderleite/catchtheballgame/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/catchtheballgame/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,12 +21,20 @@ public class MainActivity extends AppCompatActivity {
     private ImageView pink;
     private ImageView black;
 
+    //Size
+    private int frameHeight;
+    private int boxSize;
+
     //Position
     private int boxY;
 
     //Initialize Class
     private Handler handler = new Handler();
     private Timer timer = new Timer();
+
+    //Status Check
+    private boolean action_flg = false;
+    private boolean start_flg = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,35 +55,66 @@ public class MainActivity extends AppCompatActivity {
         pink.setY(-80);
         black.setX(-80);
         black.setY(-80);
-
-        startLabel.setVisibility(View.INVISIBLE);
-        boxY = 500;
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        changePos();
-                    }
-                });
-            }
-        }, 0, 20);
     }
 
     public void changePos() {
+
+        //Move Box
+        if (action_flg == true) {
+            //Touching
+            boxY -= 20;
+
+        } else {
+            //Releasing
+            boxY += 20;
+        }
+
+        //Check box position
+        if (boxY < 0) boxY = 0;
+        if (boxY > frameHeight - boxSize) boxY = frameHeight - boxSize;
 
         box.setY(boxY);
     }
 
     public boolean onTouchEvent(MotionEvent me) {
 
-        if(me.getAction() == MotionEvent.ACTION_DOWN) {
-            boxY -= 20;
-        }
-        box.setY(boxY);
+        if (start_flg == false) {
 
+            start_flg = true;
+
+            //Why get frame height and box height here?
+            //Because the UI has not been set on the screen in onCreate()!!
+
+            FrameLayout frame = (FrameLayout) findViewById(R.id.frame);
+            frameHeight = frame.getHeight();
+
+            boxY = (int)box.getY();
+
+            //The box is a square.(height and width are the same.)
+            boxSize = box.getHeight();
+
+            startLabel.setVisibility(View.GONE);
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos();
+                        }
+                    });
+                }
+            }, 0, 20);
+
+        } else {
+            if(me.getAction() == MotionEvent.ACTION_DOWN) {
+                action_flg = true;
+
+            } else if (me.getAction() == MotionEvent.ACTION_UP) {
+                action_flg = false;
+            }
+        }
         return true;
     }
 }
