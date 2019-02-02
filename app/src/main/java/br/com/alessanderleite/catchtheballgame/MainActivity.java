ca7cef42e7e9f0bf5ac3553263ce2f44d3828480
alessanderleite <alessander.logistica@gmail.com>
2019-02-02 17:13:32 -0200

    set the orange ball

--- app/src/main/java/br/com/alessanderleite/catchtheballgame/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/catchtheballgame/MainActivity.java
@@ -1,13 +1,20 @@
 package br.com.alessanderleite.catchtheballgame;
 
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -20,12 +27,28 @@ public class MainActivity extends AppCompatActivity {
     private ImageView pink;
     private ImageView black;
 
+    //Size
+    private int frameHeight;
+    private int boxSize;
+    private int screenWidth;
+    private int screenHeight;
+
     //Position
     private int boxY;
+    private int orangeX;
+    private int orangeY;
+    private int pinkX;
+    private int pinkY;
+    private int blackX;
+    private int blackY;
 
     //Initialize Class
     private Handler handler = new Handler();
     private Timer timer = new Timer();
+
+    //Status Check
+    private boolean action_flg = false;
+    private boolean start_flg = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +62,15 @@ public class MainActivity extends AppCompatActivity {
         pink = (ImageView) findViewById(R.id.pink);
         black = (ImageView) findViewById(R.id.black);
 
+        //Get screen size
+        WindowManager wm = getWindowManager();
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+
+        screenWidth = size.x;
+        screenHeight = size.y;
+
         //Move to out of screen
         orange.setX(-80);
         orange.setY(-80);
@@ -46,35 +78,75 @@ public class MainActivity extends AppCompatActivity {
         pink.setY(-80);
         black.setX(-80);
         black.setY(-80);
-
-        startLabel.setVisibility(View.INVISIBLE);
-        boxY = 500;
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        changePos();
-                    }
-                });
-            }
-        }, 0, 20);
     }
 
     public void changePos() {
+
+        //Orange
+        orangeX -= 12;
+        if (orangeX < 0) {
+            orangeX = screenWidth + 20;
+            orangeY = (int)Math.floor(Math.random() * (frameHeight - orange.getHeight()));
+        }
+        orange.setX(orangeX);
+        orange.setY(orangeY);
+
+        //Move Box
+        if (action_flg == true) {
+            //Touching
+            boxY -= 20;
+
+        } else {
+            //Releasing
+            boxY += 20;
+        }
+
+        //Check box position
+        if (boxY < 0) boxY = 0;
+        if (boxY > frameHeight - boxSize) boxY = frameHeight - boxSize;
 
         box.setY(boxY);
     }
 
     public boolean onTouchEvent(MotionEvent me) {
 
-        if(me.getAction() == MotionEvent.ACTION_DOWN) {
-            boxY -= 20;
-        }
-        box.setY(boxY);
+        if (start_flg == false) {
 
+            start_flg = true;
+
+            //Why get frame height and box height here?
+            //Because the UI has not been set on the screen in onCreate()!!
+
+            FrameLayout frame = (FrameLayout) findViewById(R.id.frame);
+            frameHeight = frame.getHeight();
+
+            boxY = (int)box.getY();
+
+            //The box is a square.(height and width are the same.)
+            boxSize = box.getHeight();
+
+            startLabel.setVisibility(View.GONE);
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos();
+                        }
+                    });
+                }
+            }, 0, 20);
+
+        } else {
+            if(me.getAction() == MotionEvent.ACTION_DOWN) {
+                action_flg = true;
+
+            } else if (me.getAction() == MotionEvent.ACTION_UP) {
+                action_flg = false;
+            }
+        }
         return true;
     }
 }
