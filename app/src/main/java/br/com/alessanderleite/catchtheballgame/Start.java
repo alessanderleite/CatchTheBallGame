b04285bd006590b702cefea75693b96f76fa306a
alessanderleite <alessander.logistica@gmail.com>
2019-02-03 17:11:18 -0200

    create the startGame method and pass the Intent inside Start class

--- app/src/main/java/br/com/alessanderleite/catchtheballgame/Start.java
+++ app/src/main/java/br/com/alessanderleite/catchtheballgame/Start.java
@@ -1,7 +1,9 @@
 package br.com.alessanderleite.catchtheballgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Start extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void startGame(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
